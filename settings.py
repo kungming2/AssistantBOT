d9ea96871f546d2f610d5a778afd677c772eb969
kungming2 <24950347+kungming2@users.noreply.github.com>
2021-04-15 23:47:00 -0700

    Make consistent to use INFO instead of AUTH

--- settings.py
+++ settings.py
@@ -12,16 +12,19 @@ import yaml
 # They should all be in the same folder as the Python script itself.
 # These addresses are then converted into a object for usage.
 SOURCE_FOLDER = os.path.dirname(os.path.realpath(__file__))
-FILE_PATHS = {"auth": '/_auth.yaml',
-              'data_main': "/_data_main.db",
-              'data_stats': "/_data_stats.db",
-              'error': "/_error.md",
-              "logs": "/_logs.md",
-              "info": "/_info.yaml",
-              'messages': "/_messages.md",
-              'messages_other': "/_messages_other.md",
-              'start': '/_start.md',
-              "settings": "/_settings.yaml"}
+FILE_PATHS = {
+    "auth": "/_auth.yaml",
+    "data_main": "/_data_main.db",
+    "data_stats": "/_data_stats.db",
+    "data_stream": "/_data_stream.db",
+    "error": "/_error.md",
+    "logs": "/_logs.md",
+    "info": "/_info.yaml",
+    "messages": "/_messages.md",
+    "messages_other": "/_messages_other.md",
+    "start": "/_start.md",
+    "settings": "/_settings.yaml",
+}
 for file_type in FILE_PATHS:
     FILE_PATHS[file_type] = SOURCE_FOLDER + FILE_PATHS[file_type]
 FILE_ADDRESS = SimpleNamespace(**FILE_PATHS)
@@ -38,12 +41,12 @@ def load_information():
     :return: A tuple containing two dictionaries, one for authentication
              data and the other with settings.
     """
-    with open(FILE_ADDRESS.info, 'r', encoding='utf-8') as f:
-        auth_data = yaml.safe_load(f.read())
-    with open(FILE_ADDRESS.settings, 'r', encoding='utf-8') as f:
+    with open(FILE_ADDRESS.info, "r", encoding="utf-8") as f:
+        info_data = yaml.safe_load(f.read())
+    with open(FILE_ADDRESS.settings, "r", encoding="utf-8") as f:
         settings_data = yaml.safe_load(f.read())
 
-    return auth_data, settings_data
+    return info_data, settings_data
 
 
 def load_instances(instance_num=99):
@@ -53,12 +56,12 @@ def load_instances(instance_num=99):
     The function defaults to the main instance if no number is
     supplied.
     """
-    with open(FILE_ADDRESS.auth, 'r', encoding='utf-8') as f:
+    with open(FILE_ADDRESS.auth, "r", encoding="utf-8") as f:
         instance_data = yaml.safe_load(f.read())
 
-    return instance_data['accounts'][instance_num]
+    return instance_data["accounts"][instance_num]
 
 
 # Retrieve credentials data needed to log in from the YAML file.
-AUTH = SimpleNamespace(**load_information()[0])
+INFO = SimpleNamespace(**load_information()[0])
 SETTINGS = SimpleNamespace(**load_information()[1])
