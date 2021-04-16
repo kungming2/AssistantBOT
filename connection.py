364cfcd58ede7edc02991f226515515a23649961
kungming2 <24950347+kungming2@users.noreply.github.com>
2021-04-15 23:54:03 -0700

    Format black

--- connection.py
+++ connection.py
@@ -11,7 +11,7 @@ import prawcore
 import yaml
 
 from common import *
-from settings import AUTH, load_instances, SETTINGS
+from settings import INFO, load_instances, SETTINGS
 
 
 """GLOBAL DEFINITIONS"""
@@ -37,23 +37,26 @@ def config_retriever():
 
     # Access the configuration page on the wiki.
     # noinspection PyUnresolvedReferences
-    target_page = reddit_helper.subreddit('translatorBOT').wiki['artemis_config'].content_md
+    target_page = reddit_helper.subreddit("translatorBOT").wiki["artemis_config"].content_md
     config_data = yaml.safe_load(target_page)
 
     # Here are some basic variables to use, making sure everything is
     # lowercase for consistency.
-    config_data['subreddits_omit'] = [x.lower().strip() for x in config_data['subreddits_omit']]
-    config_data['users_omit'] = [x.lower().strip() for x in config_data['users_omit']]
-    config_data['users_omit'] += [AUTH.creator]
-    config_data['bots_comparative'] = [x.lower().strip() for x in config_data['bots_comparative']]
-    config_data['available_instances'] = [int(x) for x in config_data['available_instances']]
+    config_data["subreddits_omit"] = [x.lower().strip() for x in config_data["subreddits_omit"]]
+    config_data["users_omit"] = [x.lower().strip() for x in config_data["users_omit"]]
+    config_data["users_omit"] += [INFO.creator]
+    config_data["bots_comparative"] = [x.lower().strip() for x in config_data["bots_comparative"]]
+    config_data["users_reply_omit"] = [x.lower().strip() for x in config_data["users_reply_omit"]]
+    config_data["sub_mention_omit"] = [x.lower().strip() for x in config_data["sub_mention_omit"]]
+    config_data["available_instances"] = [int(x) for x in config_data["available_instances"]]
+    logger.info("Config Retriever: Available: {}".format(config_data["available_instances"]))
 
     # This is a custom phrase that can be included on all wiki pages as
     # an announcement from the bot creator.
-    if 'announcement' in config_data:
+    if "announcement" in config_data:
         # Format it properly as a header with an emoji.
-        if config_data['announcement'] is not None:
-            config_data['announcement'] = "📢 *{}*".format(config_data['announcement'])
+        if config_data["announcement"] is not None:
+            config_data["announcement"] = "📢 *{}*".format(config_data["announcement"])
     CONFIG = SimpleNamespace(**config_data)
 
     return
@@ -71,29 +74,33 @@ def get_posts_frequency():
     global NUMBER_TO_FETCH
 
     # If not deployed on Linux, we can use a set number instead.
-    if not sys.platform.startswith('linux'):
+    if not sys.platform.startswith("linux"):
         NUMBER_TO_FETCH = SETTINGS.min_get_posts
-        logger.info('Get Posts Frequency: Testing on `{}`. '
-                    'Limit set to minimum.'.format(sys.platform))
+        logger.info(
+            "Get Posts Frequency: Testing on `{}`. " "Limit set to minimum.".format(sys.platform)
+        )
         return
 
     # 15 minutes is our interval to test for.
     # Begin processing from the oldest post.
     time_interval = SETTINGS.min_monitor_sec * 3
     # noinspection PyUnresolvedReferences
-    posts = list(reddit.subreddit('mod').new(limit=SETTINGS.max_get_posts))
+    posts = list(reddit.subreddit("mod").new(limit=SETTINGS.max_get_posts))
     posts.reverse()
 
     # Take the creation time of the oldest post and calculate the
     # interval between that and now. Then get the average time period
     # for posts to come in and the nominal amount of posts that come in
     # within our interval.
-    time_difference = (int(time.time()) - int(posts[0].created_utc))
+    time_difference = int(time.time()) - int(posts[0].created_utc)
     interval_between_posts = time_difference / SETTINGS.max_get_posts
     boundary_posts = int(time_interval / interval_between_posts)
-    logger.info('Get Posts Frequency: {:,} posts came in the last {:.2f} '
-                'minutes. New post every {:.2f} seconds.'.format(len(posts), time_difference / 60,
-                                                                 interval_between_posts))
+    logger.info(
+        "Get Posts Frequency: {:,} posts came in the last {:.2f} "
+        "minutes. New post every {:.2f} seconds.".format(
+            len(posts), time_difference / 60, interval_between_posts
+        )
+    )
 
     # Next we determine how many posts Artemis should *fetch* in a 15
     # minute period defined by the data. That number is 2 times the
@@ -106,19 +113,29 @@ def get_posts_frequency():
     # If we need to adjust the broader limit, note that. Also make sure
     # the number to fetch is always at least our minimum.
     if SETTINGS.max_get_posts < NUMBER_TO_FETCH:
-        logger.info('Get Posts Frequency: The broader limit of {} posts '
-                    'may need to be higher.'.format(SETTINGS.max_get_posts))
+        logger.info(
+            "Get Posts Frequency: The broader limit of {} posts "
+            "may need to be higher.".format(SETTINGS.max_get_posts)
+        )
     elif NUMBER_TO_FETCH < SETTINGS.min_get_posts:
         NUMBER_TO_FETCH = int(SETTINGS.min_get_posts)
-        logger.info('Get Posts Frequency: Limit set to '
-                    'minimum limit of {} posts.'.format(SETTINGS.min_get_posts))
+        logger.info(
+            "Get Posts Frequency: Limit set to "
+            "minimum limit of {} posts.".format(SETTINGS.min_get_posts)
+        )
     else:
-        logger.info('Get Posts Frequency: Adjusted: '
-                    '{} posts / {} minutes.'.format(NUMBER_TO_FETCH, int(time_interval / 60)))
-        logger.info('Get Posts Frequency: Actual: {:.2f} posts '
-                    'per minute.'.format(NUMBER_TO_FETCH / 120))
-        logger.info('Get Posts Frequency: {:.0f} posts per '
-                    'isochronism per section.'.format(NUMBER_TO_FETCH / SETTINGS.num_chunks))
+        logger.info(
+            "Get Posts Frequency: Adjusted: "
+            "{} posts / {} minutes.".format(NUMBER_TO_FETCH, int(time_interval / 60))
+        )
+        logger.info(
+            "Get Posts Frequency: Actual: {:.2f} posts "
+            "per minute.".format(NUMBER_TO_FETCH / 120)
+        )
+        logger.info(
+            "Get Posts Frequency: {:.0f} posts per "
+            "isochronism per section.".format(NUMBER_TO_FETCH / SETTINGS.num_chunks)
+        )
 
     return
 
@@ -145,27 +162,31 @@ def login(posts_frequency=True, instance_num=99):
     instance_login = SimpleNamespace(**load_instances(instance_num))
 
     # Format the instance.
-    user_agent = 'Artemis v{} (u/{}), a moderation assistant written by u/{}.'
+    user_agent = "Artemis v{} (u/{}), a moderation assistant written by u/{}."
     if instance_num != 99:
-        username_adapted = "{}{}".format(AUTH.username, instance_num)
+        username_adapted = "{}{}".format(INFO.username, instance_num)
     else:
-        username_adapted = AUTH.username
-    user_agent = user_agent.format(AUTH.version_number, username_adapted,
-                                   AUTH.creator)
+        username_adapted = INFO.username
+    user_agent = user_agent.format(INFO.version_number, username_adapted, INFO.creator)
 
     # Authenticate the main connection.
-    reddit = praw.Reddit(client_id=instance_login.app_id,
-                         client_secret=instance_login.app_secret,
-                         password=instance_login.password,
-                         user_agent=user_agent, username=username_adapted)
+    reddit = praw.Reddit(
+        client_id=instance_login.app_id,
+        client_secret=instance_login.app_secret,
+        password=instance_login.password,
+        user_agent=user_agent,
+        username=username_adapted,
+    )
     logger.info("Startup: Logging in as u/{}.".format(username_adapted))
 
     # Authenticate the secondary helper connection.
-    reddit_helper = praw.Reddit(client_id=AUTH.helper_app_id,
-                                client_secret=AUTH.helper_app_secret,
-                                password=AUTH.helper_password,
-                                user_agent="{} Assistant".format(AUTH.username),
-                                username=AUTH.helper_username)
+    reddit_helper = praw.Reddit(
+        client_id=INFO.helper_app_id,
+        client_secret=INFO.helper_app_secret,
+        password=INFO.helper_password,
+        user_agent="{} Assistant".format(INFO.username),
+        username=INFO.helper_username,
+    )
 
     # Access configuration data.
     config_retriever()
@@ -199,9 +220,9 @@ def obtain_mod_permissions(subreddit_name, instance_num=99):
     r = reddit.subreddit(subreddit_name)
 
     if instance_num != 99:
-        check_username = "{}{}".format(AUTH.username.lower(), instance_num)
+        check_username = "{}{}".format(INFO.username.lower(), instance_num)
     else:
-        check_username = AUTH.username.lower()
+        check_username = INFO.username.lower()
 
     # This is a try/except sequence to account for private subreddits
     # since one is unable to get a mod list from a private one.
@@ -237,19 +258,19 @@ def obtain_subreddit_public_moderated(username):
     # Iterate through the data and get the subreddit names and their
     # Reddit fullnames (prefixed with `t5_`). It will fail gracefully
     # if the account does not moderate anything.
-    mod_target = '/user/{}/moderated_subreddits'.format(username)
+    mod_target = "/user/{}/moderated_subreddits".format(username)
     try:
-        for subreddit in reddit_helper.get(mod_target)['data']:
-            active_subreddits.append(subreddit['sr'].lower())
-            active_fullnames.append(subreddit['name'].lower())
+        for subreddit in reddit_helper.get(mod_target)["data"]:
+            active_subreddits.append(subreddit["sr"].lower())
+            active_fullnames.append(subreddit["name"].lower())
     except KeyError:  # This username does not moderate anything.
         return {}
     else:
         active_subreddits.sort()
 
-    subreddit_dict['list'] = active_subreddits
-    subreddit_dict['fullnames'] = active_fullnames
-    subreddit_dict['total'] = len(active_subreddits)
+    subreddit_dict["list"] = active_subreddits
+    subreddit_dict["fullnames"] = active_fullnames
+    subreddit_dict["total"] = len(active_subreddits)
 
     return subreddit_dict
 
@@ -266,18 +287,19 @@ def messaging_send_creator(subreddit_name, subject_type, message):
     """
     # This is a dictionary that defines what the subject line will be
     # based on the action. The add portion is currently unused.
-    subject_dict = {"add": 'Added former subreddit: r/{}',
-                    "remove": "Demodded from subreddit: r/{}",
-                    "forbidden": "Subscribers forbidden for subreddit: r/{}",
-                    "not_found": "Subscribers not found for subreddit: r/{}",
-                    "omit": "Omitted subreddit: r/{}",
-                    "mention": "New item mentioning Artemis on r/{}"
-                    }
+    subject_dict = {
+        "add": "Added former subreddit: r/{}",
+        "remove": "Demodded from subreddit: r/{}",
+        "forbidden": "Subscribers forbidden for subreddit: r/{}",
+        "not_found": "Subscribers not found for subreddit: r/{}",
+        "omit": "Omitted subreddit: r/{}",
+        "mention": "New item mentioning Artemis on r/{}",
+    }
 
     # If we have a matching subject type, send a message to the creator.
     if subject_type in subject_dict:
         # noinspection PyUnresolvedReferences
-        creator = reddit.redditor(AUTH.creator)
+        creator = reddit.redditor(INFO.creator)
         creator.message(subject=subject_dict[subject_type].format(subreddit_name), message=message)
 
     return
@@ -291,7 +313,7 @@ def monitored_subreddits_enforce_mode(subreddit_name, instance_num=99):
     :param instance_num: Instance number of a subreddit.
     :return: The Artemis mode of the subreddit as a string.
     """
-    enforce_mode = 'Default'
+    enforce_mode = "Default"
     enhancement = ""
 
     # Get the type of flair enforcing default/strict status.
@@ -301,19 +323,19 @@ def monitored_subreddits_enforce_mode(subreddit_name, instance_num=99):
     # If I am a moderator, check for the `+` enhancement and then for
     # strict mode. Return `N/A` if not a moderator.
     if current_permissions[0]:
-        if 'flair' in current_permissions[1] or 'all' in current_permissions[1]:
+        if "flair" in current_permissions[1] or "all" in current_permissions[1]:
             enhancement = "+"
-        if 'posts' in current_permissions[1] or 'all' in current_permissions[1]:
-            enforce_mode = 'Strict'
+        if "posts" in current_permissions[1] or "all" in current_permissions[1]:
+            enforce_mode = "Strict"
         flair_enforce_status = enforce_mode + enhancement
     else:
-        flair_enforce_status = 'N/A'
+        flair_enforce_status = "N/A"
 
     return flair_enforce_status
 
 
 def monitored_instance_checker(query_subreddit=None, max_num=9):
-    """ This function checks across all instances to see all the
+    """This function checks across all instances to see all the
     subreddits moderated. This is important for avoiding duplicate
     additions to a moderation team.
 
@@ -327,12 +349,12 @@ def monitored_instance_checker(query_subreddit=None, max_num=9):
     """
     full_dict = {}
     accounts_on = []
-    usernames = [AUTH.username, 'AssistantBOT0']
+    usernames = [INFO.username, "AssistantBOT0"]
     count = 1
 
     # Create a list of the accounts to check.
     while count <= max_num:
-        usernames.append("{}{}".format(AUTH.username, count))
+        usernames.append("{}{}".format(INFO.username, count))
         count += 1
     logger.debug("Usernames to check for instances: {}".format(usernames))
 
@@ -346,21 +368,25 @@ def monitored_instance_checker(query_subreddit=None, max_num=9):
     # just return the data dictionary.
     if query_subreddit:
         query_subreddit = query_subreddit.lower()
-        logger.info('Instance Checker: Searching for subreddit r/{}'.format(query_subreddit))
+        logger.info("Instance Checker: Searching for subreddit r/{}".format(query_subreddit))
         for account in full_dict:
-            if 'list' in full_dict[account]:
-                if query_subreddit in full_dict[account]['list']:
+            if "list" in full_dict[account]:
+                if query_subreddit in full_dict[account]["list"]:
                     accounts_on.append(account)
             else:
                 continue
 
         if accounts_on:
-            logger.info('Instance Checker: Subreddit r/{} is '
-                        'already on instances: {}'.format(query_subreddit, accounts_on))
+            logger.info(
+                "Instance Checker: Subreddit r/{} is "
+                "already on instances: {}".format(query_subreddit, accounts_on)
+            )
             return True, accounts_on
         else:
-            logger.info('Instance Checker: Subreddit r/{} is not '
-                        'moderated by any instance.'.format(query_subreddit))
+            logger.info(
+                "Instance Checker: Subreddit r/{} is not "
+                "moderated by any instance.".format(query_subreddit)
+            )
             return False, []
     else:
         logger.info("Instance Checker: No subreddit query. Dictionary returned.")
